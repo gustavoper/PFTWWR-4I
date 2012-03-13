ee1a83601aa5db66e96e9ff96ca7b74ee4f8bbb1
gustavogpe <gustavoper@gmail.com>
2012-03-13 08:56:19 -0300

    Exercise 7 (last try. Works on Windows 7/x64

--- Exercise_7/my_rack_proc_2.rb
+++ Exercise_7/my_rack_proc_2.rb
@@ -3,16 +3,23 @@ require 'rack'
 
 
 
+
 def simple_rack_call(str)
 	my_rack_proc = lambda { |env| [200, {"Content-Type" => "text/plain"}, ["Command Line argument you typed is: #{str}"]] }
-	puts my_rack_proc.call({})
+	#puts my_rack_proc.call({})
 end
 
 
 
 	if (ARGV[0])
 		#starting WEBrick server...
-		Rack::Handler::WEBrick.run simple_rack_call(ARGV[0]),:Port =>8500
+		#Rack::Handler::WEBrick.run simple_rack_call(), :Port =>8500
+		#Rack::Server.new( { :app=>simple_rack_call, :server=>'webrick', :Port =>8500 } ).start
+
+		server = Rack::Server.new( { :app => simple_rack_call(ARGV[0]), :server => 'webrick', :Port => 8500 } )
+		server.start
+
+
 	else 
 		puts "Invalid arguments!"
 	end
